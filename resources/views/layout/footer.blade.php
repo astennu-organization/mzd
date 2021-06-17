39f7f256352e3250ba3a78b907f3736600faca8c
Kristijan Stojcev <stojcev_kristijan@hotmail.com>
2021-06-17 16:15:57 +0200

    Added front-end

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid p-5">
+    <div class="row">
+      <div class="col-md-12 text-center" style="font-family: sans-serif">
+        © 2010 Мрежа за заштита од дискриминација
+      </div>
+    </div>
+</footer> 
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,48 @@
+<header class="custom-navbar fixed-top">
+    <div class="custom-navbrand">
+      <div class="custom-brand">МЗД</div>
+      <div class="custom-burger" id="burger">
+        <span class="custom-burger-open">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="16">
+            <g fill="#252a32" fill-rule="evenodd">
+              <path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z" />
+            </g>
+          </svg>
+        </span>
+        <span class="custom-burger-close">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+            <path
+              fill="#252a32"
+              fill-rule="evenodd"
+              d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z"
+            />
+          </svg>
+        </span>
+      </div>
+    </div>
+    <ul class="custom-menu m-auto" id="menu">
+      <li class="custom-menu-item">
+        <a href="#" class="custom-menu-link custom-active">Почетна</a>
+      </li>
+      <li class="custom-menu-item">
+        <a href="#" class="custom-menu-link">За нас</a>
+      </li>
+      <li class="custom-menu-item">
+        <a href="#" class="custom-menu-link">Новости</a>
+      </li>
+      <li class="custom-menu-item">
+        <a href="#" class="custom-menu-link">Галерија</a>
+      </li>
+      <li class="custom-menu-item">
+        <a href="#" class="custom-menu-link">Пријави случај</a>
+      </li>
+    </ul>
+    <div class="custom-social">
+      <a href="#" class="custom-social-item"
+        ><i class="fab fa-facebook"></i
+      ></a>
+      <a href="#" class="custom-social-item"
+        ><i class="fab fa-twitter"></i
+      ></a>
+    </div>
+  </header>
